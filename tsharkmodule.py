56af1902fed4ef85177aca750df9b2969d39222a
Hrishikesh Deepak Kakkad <hrishidkakkad@outlook.com>
2020-01-23 21:08:07 +0530

    Update tsharkmodule.py

--- tsharkmodule.py
+++ tsharkmodule.py
@@ -1,8 +1,6 @@
 import os
-#sudo tshark -V -i wlan0mon -n -Y ((wlan.fc.type == 0)&&(wlan.fc.type_subtype == 0x0c))
-#sudo tshark -V -i wlan0mon -n -Y "((wlan.fc.type == 0)&&(wlan.fc.type_subtype == 0x0c))" > deauthlog.txt
+
 os.system('cp /dev/null ./text/logtest.txt')
 os.system('cp /dev/null ./text/deauthlog.txt')
 os.system('sudo tshark -V -i wlan1mon -n -Y "((wlan.fc.type == 0)&&(wlan.fc.type_subtype == 0x0c))" |tee -a ./text/log.txt ./text/deauthlog.txt ./text/logtest.txt > /dev/null')
-#os.system('sudo tshark -V -i wlan0mon -n -Y "((wlan.fc.type == 0)&&(wlan.fc.type_subtype == 0x0c))" > deauthlog.txt')
-#os.system('clear')
+
